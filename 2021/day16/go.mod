ffc50ab72dcedea4054a2e4d7869cfa1b8c12949
Chris Baker <cgbaker@amazon.com>
2021-12-16 14:39:48 -0600

    day 16 is done

--- 2021/day16/go.mod
+++ 2021/day16/go.mod
@@ -1,3 +1,8 @@
 module github.com/cgbaker/AOC/2021/day16
 
 go 1.17
+
+require (
+	github.com/bearmini/bitstream-go v0.0.0-20190121230027-bec1c9ea0d3c // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+)
--- 2021/day16/main.go
+++ 2021/day16/main.go
@@ -2,124 +2,178 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/hex"
 	"fmt"
+	bitstream "github.com/bearmini/bitstream-go"
 	"log"
+	"math"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("sample1.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
-	packet := readPacket(file)
-	fmt.Println(packet)
-
-	fmt.Println("Part1:", packet.sumOfVersions())
-	// fmt.Println("Part2:", packet.???)
-}
-
-func readPacket(file *os.File) *Packet {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	bytes := []byte{}
-	scanner.Scan()
-	str := scanner.Text()
-	if len(str) % 2 != 0 {
-		str += "0"
-	}
-	bytes, err := hex.DecodeString(str)
-	if err != nil {
-		panic("parsing: " + err.Error())
-	}
-	return &Packet{
-		bytes: bytes,
+	for scanner.Scan() {
+		str := scanner.Text()
+		if len(str) < 80 {
+			fmt.Println("Input:",str)
+		}
+		if len(str) % 2 != 0 {
+			str += "0"
+		}
+		byts, err := hex.DecodeString(str)
+		if err != nil {
+			panic("parsing: " + err.Error())
+		}
+		r := bitstream.NewReader(bytes.NewReader(byts), nil)
+		packet, _ := readPacket(r)
+		fmt.Println("Part1:", sumOfVersions(packet))
+		fmt.Println("Part2:", packet.value())
 	}
 }
 
 type Packet struct {
 	version int
-	typeId  int
-	subPackets []Packet
+	typeId  PacketType
+	literal int64
+	subPackets []*Packet
 }
 
-type Type byte
+type PacketType int
 const (
-	LITERAL Type = 4
+	SUM      PacketType = 0
+	PRODUCT  PacketType = 1
+	MINIMUM  PacketType = 2
+	MAXIMUM  PacketType = 3
+	LITERAL  PacketType = 4
+	GRTRTHAN PacketType = 5
+	LESSTHAN PacketType = 6
+	EQUALTO  PacketType = 7
 )
 
-func (p *Packet) getSubPackets() []Packet {
-	if p.getType() == LITERAL {
-		return nil
+func readPacket(r *bitstream.Reader) (*Packet, int) {
+	p := &Packet{
+		version: readNBits(r, 3),
+		typeId:  PacketType(readNBits(r, 3)),
 	}
-	//var subPacketBytes []byte
-	lenType := p.getLengthType()
-	length := 0
-	shift := 0
-	switch lenType {
-	case 0:
-		// need 15 == 1 + 8 + 6
-		length = int(0b00000001 & p.bytes[0]) << 14
-		length += int(p.bytes[1]) << 6
-		length += int(p.bytes[2] & 0b11111100) >> 2
-		shift = 22
-	case 1:
-		// need 11 == 1 + 8 + 2
-		length = int(0b00000001 & p.bytes[0]) << 10
-		length += int(p.bytes[1]) << 2
-		length += int(p.bytes[2] & 0b11000000) >> 6
-		shift = 18
-	}
-	subPacketBytes := shiftBytes(p.bytes, shift)
-	return dividePackets(subPacketBytes, lenType, length)
-}
-
-func shiftBytes(bytes []byte, shift int) []byte {
-	shiftBytes := shift / 8
-	shiftBits := shift % 8
-	trunc := bytes[shiftBytes:]
-	if shiftBits == 0 {
-		return trunc
-	}
-	shifted := make([]byte,0,len(trunc))
-	high := 0
-	low := 0
-	for i, t := range trunc {
-		high = ((int(t) << shiftBits) & 0xff00) >> 8
-		if i > 0 {
-			if high | low > 255 {
-				panic("too big")
+	bitsRead := 6
+	if p.typeId == LITERAL {
+		numNibbles := 0
+		for {
+			numNibbles++
+			next := readNBits(r, 5)
+			bitsRead += 5
+			p.literal = (p.literal << 4) + int64(next & 0b1111)
+			if next & 0b10000 == 0 {
+				break
+			} else if numNibbles >= 16 {
+				panic("nibbled too much; need to use something bigger than int64")
 			}
-			shifted = append(shifted, byte(high | low))
 		}
-		low = int(t) << shiftBits & 255
+	} else {
+		lengthType, _ := r.ReadBit()
+		bitsRead++
+		switch lengthType {
+		case 0:
+			length := readNBits(r, 15)
+			bitsRead += 15
+			for {
+				if length == 0 {
+					break
+				} else if length < 0 {
+					panic("i read too much")
+				}
+				subP, read := readPacket(r)
+				length -= read
+				bitsRead += read
+				p.subPackets = append(p.subPackets, subP)
+			}
+		case 1:
+			length := readNBits(r, 11)
+			bitsRead += 11
+			for i := 0; i < length; i++ {
+				subP, read := readPacket(r)
+				bitsRead += read
+				p.subPackets = append(p.subPackets, subP)
+			}
+		}
 	}
-	shifted = append(shifted, byte(low))
-	return shifted
+	return p, bitsRead
 }
 
-func (p *Packet) sumOfVersions() int {
-	sum := int(p.getVersion())
-	for _, sub := range p.getSubPackets() {
-		sum += sub.sumOfVersions()
+func (p *Packet) value() int64 {
+	value := int64(0)
+	switch p.typeId {
+	case SUM:
+		for _, p := range p.subPackets {
+			value += p.value()
+		}
+	case PRODUCT:
+		value = 1
+		for _, p := range p.subPackets {
+			value *= p.value()
+		}
+	case MINIMUM:
+		value = math.MaxInt
+		for _, p := range p.subPackets {
+			value = min(value, p.value())
+		}
+	case MAXIMUM:
+		for _, p := range p.subPackets {
+			value = max(value, p.value())
+		}
+	case LITERAL:
+		value = p.literal
+	case GRTRTHAN:
+		if p.subPackets[0].value() > p.subPackets[1].value() {
+			value = 1
+		}
+	case LESSTHAN:
+		if p.subPackets[0].value() < p.subPackets[1].value() {
+			value = 1
+		}
+	case EQUALTO:
+		if p.subPackets[0].value() == p.subPackets[1].value() {
+			value = 1
+		}
+	default:
+		panic("i'm sorry, sir, this is a wendy's")
+	}
+	return value
+}
+
+func sumOfVersions(p *Packet) int {
+	sum := p.version
+	for _, sub := range p.subPackets {
+		sum += sumOfVersions(sub)
 	}
 	return sum
 }
 
-func (p *Packet) getVersion() byte {
-	return (p.bytes[0] & 0b11100000) >> 5
-}
-
-func (p *Packet) getType() Type {
-	return Type(p.bytes[0] & 0b00011100 >> 2)
-}
-
-func (p *Packet) getLengthType() byte {
-	if p.getType() == LITERAL {
-		panic("no length type")
+func readNBits(r *bitstream.Reader, n uint8) int {
+	i, err := r.ReadNBitsAsUint32BE(n)
+	if err != nil {
+		panic(err.Error())
 	}
-	return (p.bytes[0] & 0b00000010) >> 1
+	return int(i)
+}
+
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int64) int64 {
+	if a < b {
+		return b
+	}
+	return a
 }
