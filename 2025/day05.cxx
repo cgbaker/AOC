810d76163b4c74589a4c1aa68c8568919523ff84
Chris Baker <1675087+cgbaker@users.noreply.github.com>
2025-12-05 18:12:56 -0600

    day 05, part 2

--- 2025/day05.cxx
+++ 2025/day05.cxx
@@ -1,83 +1,84 @@
 #include <sstream>
 #include <string>
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <list>
 #include <numeric>
 #include <ranges>
 #include <functional>
 
 using std::cout;
 using std::endl;
+using std::list;
 using std::pair;
 using std::string;
-using std::vector;
 
 typedef int64_t ID;
 typedef std::pair<int64_t, int64_t> Range;
-auto cmp = [](const Range &r1, const Range &r2)
-{
-    return r1.second < r2.first || r1.first < r2.first;
-};
 auto contains = [](const Range &range, ID id)
 {
     return range.first <= id && id <= range.second;
 };
 auto overlaps = [](const Range &r1, const Range &r2)
 {
-    return contains(r1, r2.first) || contains(r1, r1.second);
+    return r1.first <= r2.second && r2.first <= r1.second;
 };
 
 int main()
 {
-    // nothing clever for now, just keep a vector of the ranges
-    vector<Range> ranges;
-    ID part1 = 0;
+    list<Range> ranges;
     string line;
     while (std::getline(std::cin, line))
     {
         if (line.empty())
             break;
-
         std::stringstream ss(line);
         ID begin, end;
         char dash;
         ss >> begin >> dash >> end;
-        Range r(begin, end);
-        auto it = std::lower_bound(ranges.begin(), ranges.end(), r, cmp);
-        if (it != ranges.end() && overlaps(*it, r))
+        ranges.push_back(Range(begin, end));
+    }
+
+    ranges.sort([](const Range &r1, const Range &r2) { 
+        return r1.first < r2.first || (r1.first == r2.first && r1.second < r2.second); 
+    });
+
+    auto iter = ranges.begin();
+    auto next(iter);
+    ++next;
+    while (next != ranges.end())
+    {
+        if (overlaps(*iter, *next))
         {
-            cout << std::format("Combining [{},{}] and [{},{}]",r.first,r.second,it->first,it->second) << endl;
-            (*it).first = std::min(it->first, r.first);
-            (*it).second = std::max(it->second, r.second);
+            Range combined( std::min(iter->first, next->first), std::max(iter->second, next->second) );
+            (*iter) = combined;
+            next = ranges.erase(next);
         }
         else
         {
-            ranges.insert(it, r);
+            ++iter;
+            next = iter;
+            ++next;
         }
     }
 
-    auto reducer = [&ranges](ID id)
+    auto finder = [&ranges](ID id)
     {
         auto it = std::find_if(ranges.begin(), ranges.end(), [id](const Range &r)
                                { return contains(r, id); });
         return it != ranges.end();
     };
 
-    // we got the first one above
     ID id;
+    ID part1 = 0;
     while (std::cin >> id)
     {
-        if (reducer(id))
+        if (finder(id))
             part1++;
     };
 
-    std::for_each(ranges.begin(), ranges.end(), [](const Range &r){
-        cout << std::format("[{},{}]", r.first, r.second) << endl;
-    });
-    ID part2 = std::accumulate(ranges.begin(), ranges.end(), ID(0), [](ID acc, const Range &r) {
-        return acc + r.second - r.first + 1;
-    });
+    ID part2 = std::accumulate(ranges.begin(), ranges.end(), ID(0), [](ID acc, const Range &r)
+                               { return acc + r.second - r.first + 1; });
 
     cout << "Part 1: " << part1 << endl;
     cout << "Part 2: " << part2 << endl;
